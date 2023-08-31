ef7cc1219d7445f3577602d434c6dd8976614e52
Jason Clare <70923049+mintyModulo@users.noreply.github.com>
2023-08-31 06:30:09 -0500

    Finished

---
+++ Chapter_2/Exercise_2.15_3.py
@@ -0,0 +1,10 @@
+# Write a program to prompt the user for hours and rate per hour to computer gross pay.
+# Enter Hours: 35
+# Enter Rate: 2.75
+# Pay: 96.25
+
+hours = input('Enter Hours: ')
+rate = input('Enter Rate: ')
+pay = float(hours) * float(rate)
+
+print('Pay:', pay)
