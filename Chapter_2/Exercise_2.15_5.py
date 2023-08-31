ed93f212305ad4b333b9ee57989f11acc1f11957
Jason Clare <70923049+mintyModulo@users.noreply.github.com>
2023-08-31 06:30:56 -0500

    Finished

---
+++ Chapter_2/Exercise_2.15_5.py
@@ -0,0 +1,7 @@
+# Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+
+celsius = input('Temperature in Celsius: ')
+
+fahrenheit = ((float(celsius) * (9 /5 )) + 32)
+
+print('Temperature in Fahrenheit:', fahrenheit)
